8e5f6ee86b3545e648403339911d824cb12ddbc8
Anjas Dwi <anjasdwi@bareksa.com>
2024-03-14 19:35:39 +0700

    [helper]<improvement>: Add constant folder and file

---
+++ SimpleMovieAnjas/Constant/Image.swift
@@ -0,0 +1,12 @@
+//
+//  IMAGE.swift
+//  SimpleMovieAnjas
+//
+//  Created by Anjas Dwi on 14/03/24.
+//
+
+import Foundation
+
+enum IMAGE {
+    static let home = "ic_home"
+}
---
+++ SimpleMovieAnjas/Constant/Wording.swift
@@ -0,0 +1,12 @@
+//
+//  Wording.swift
+//  SimpleMovieAnjas
+//
+//  Created by Anjas Dwi on 14/03/24.
+//
+
+import Foundation
+
+enum WORDING {
+    static let home = "Home"
+}
